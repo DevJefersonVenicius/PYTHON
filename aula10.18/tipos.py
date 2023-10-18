3ecbf56c7f4e90e80c11851a3384aede6c1a2e9f
Jeferson Venicius <veniciusvieira90@gmail.com>
2023-10-18 15:34:45 -0300

    Alterado o arquivo tipos.py

--- aula10.18/tipos.py
+++ aula10.18/tipos.py
@@ -2,3 +2,7 @@ a = 10 # inteiro
 b = 10.0 # no python número real é denominado (float)
 c = "10" # no python texto é denominado (string)
 d = "10.0" # string
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
