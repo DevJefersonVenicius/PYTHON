c0947e5db71a92b4a3c86e9b247b773c85ec4faf
Jeferson Venicius <veniciusvieira90@gmail.com>
2023-10-18 16:19:03 -0300

    Adiconado o arquivo desafio02.py

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,9 @@
+idade = input('Digite sua idade aqui: ')
+
+idade_parente = input('Digite a idade do parente: ')
+
+nome_usuario = input('Digite o seu nome: ')
+
+print(int(idade) * int(idade_parente))
+
+print(nome_usuario)
