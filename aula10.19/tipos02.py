eb9cd21bbf08dde08618ba4bf2254ab3c705f4bb
Jeferson Venicius <veniciusvieira90@gmail.com>
2023-10-19 15:34:16 -0300

    Conteúdo sobre tipos adicionado

---
+++ aula10.19/tipos02.py
@@ -0,0 +1,13 @@
+# Entender tipos e mudanças de tipos dentro do python
+
+a = 50
+b = "50"
+print(type(a))
+print(type(b))
+
+idade = int(input('Qual sua idade? '))
+print(type(idade))
+
+# utilização do float: moedas, peso, temperatura, medidas e outros casos parecidos
+peso = float(input('Qual seu peso? '))
+print(type(peso))
