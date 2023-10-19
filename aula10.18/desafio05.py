42c18523f09b4224844de408b6d7b5075ed40f0e
Jeferson Venicius <veniciusvieira90@gmail.com>
2023-10-19 11:48:14 -0300

    Alterado o arquivo desafio05.py

--- aula10.18/desafio05.py
+++ aula10.18/desafio05.py
@@ -1,3 +1,2 @@
 metros = input('Digite o total de metros a ser convertido: ')
-conversao_cm = 100
-print('O resultado da conversão é igual a: ', (int(metros)) * (conversao_cm))
+print('O resultado da conversão é igual a: ', (int(metros)) * 100)
