538962c527599e5b2aae78307b4f2fd3f1de1850
Jeferson Venicius <veniciusvieira90@gmail.com>
2023-10-21 19:07:16 -0300

    Criado calculo_peso_ideal.py

---
+++ aula10.19/calculo_peso_ideal.py
@@ -0,0 +1,4 @@
+h_m = float(input('Digite sua altura: '))
+h_f = float(input('Digite sua altura: '))
+print(f'De acordo com sua altura de {h_m}, seu peso ideal é: ', (72.7 * h_m) - 58)
+print(f'De acordo com sua altura de {h_f}, seu peso ideal é: ', (62.1 * h_f) - 44.7)
