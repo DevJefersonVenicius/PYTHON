ef17f574708ac7afda5d5b173a89541a723c384c
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-01 12:50:21 -0300

    vogal_consoante.py deletado

--- vogal_consoante.py
+++
@@ -1,8 +0,0 @@
-vogal = input('Digite a letra: ')
-consoante = input('Digite a letra: ')
-
-if vogal == "a" or vogal == "e" or vogal == "i" or vogal == "o" or vogal == "u":
-    print('A vogal digitada é: ', vogal)
-else:
-    consoante != vogal
-    print('A consoante digitada é: ', consoante)
