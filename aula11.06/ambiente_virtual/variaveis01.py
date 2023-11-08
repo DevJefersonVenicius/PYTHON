77bf2829035ba38c2d8f0d09d5d82a1bfcb83ab5
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-08 11:54:35 -0300

    .gitignore adicionado

---
+++ aula11.06/ambiente_virtual/variaveis01.py
@@ -0,0 +1,15 @@
+# variaveis são usadas para salvar algo na  memoria do computador
+# o sinal de = é utilizado para atribuição de valores
+# os valores de uma variavel são mutaveis
+var01 = "alguma coisa"
+print(var01)
+var01 = "outra coisa"
+print(var01)
+var01 = 10
+print(var01)
+var01 = "A" + "B" + "C"
+print(var01)
+var01 = "A" * 10
+print(var01)
+var02 = 10 * "Jeferson "
+print(var02)
