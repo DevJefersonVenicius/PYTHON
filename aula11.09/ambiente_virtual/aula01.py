635fe7674aa7f706533910cab983b3cb716931a3
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-10 13:33:00 -0300

    Adicionado conteúdo no arquivo aula01.py

--- aula11.09/ambiente_virtual/aula01.py
+++ aula11.09/ambiente_virtual/aula01.py
@@ -1,11 +1,26 @@
-nome = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhando entre participantes, instrutores e empresas parceiras."
+nome = "Somos uma escola de tecnologia e informação que cria pontes entre pessoas, conhecimento e empresas.\
+Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhando entre participantes,\
+instrutores e empresas parceiras."
 print(nome[20:30])
-print(nome[34:44])
+print(nome[33:44])
 print(nome[10:16])
-print(nome[54:60])
+print(nome[53:60])
 print(nome[-10:-1])
 print(nome[-19:-11])
 print(nome[-33:-21])
-print(nome[61:66])
-print(nome[67:74])
-print(nome[76:88])
+print(nome[60:66])
+print(nome[66:73])
+print(nome[75:88])
+
+nome2 = "Somos uma escola de tecnologia e informação que cria pontes entre pessoas, conhecimento e empresas.\
+Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhando entre participantes,\
+instrutores e empresas parceiras."
+print("="*20)
+print(nome2[0:5])
+print(nome[6:9])
+print(nome[10:16])
+print(nome[17:19])
+print(nome[20:30])
+print(nome[44:48])
+print(nome[-105:-100])
+print(nome[-96:-83])
