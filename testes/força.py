d4a3f3860849e92805b6d4ed446efea695fe104f
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-14 13:46:55 -0300

    Adicionado o arquivo força.py

---
+++ testes/força.py
@@ -0,0 +1,4 @@
+massa = float(input("Digite a massa em kg: "))
+aceleraçao = 10 
+força = massa * aceleraçao
+print(f'A força de um objeto com massa igual a {massa}kg e sua acelaração igual a {aceleraçao}m/s é: {força}')
