8c021c33c0a90b3a5f7c9e42f783d4432d466337
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-14 12:32:45 -0300

    Adicionados os arquivos aula01.py e lista.py

---
+++ aula11.13/ambiente_virtual/aula01.py
@@ -0,0 +1,10 @@
+lista = ['Jeferson Lucas', 'Mikael', 'Lara', 'Junior', 'Kaynan']
+print(lista)
+lista.append('Elvis')
+print(lista)
+del lista[2]
+print(lista)
+lista.remove('Mikael')
+print(lista)
+lista.append('Paulo')
+print(lista)
---
+++ aula11.13/ambiente_virtual/lista.py
@@ -0,0 +1,57 @@
+# uma lista é representada pelos []
+# len - metodo que retorna a quantidade de itens de uma lista
+# append - metodo que insere itens no final da lista
+# del - remove pelo indice especifico da lista
+# remove - remove um objeto especificado da lista
+# pop - remove o ultimo elemento da lista
+# insert - adiciona um objeto no inico da lista
+'''
+Lista = []
+print(lista, type(lista))
+print(len(lista))
+lista = ['front']
+print(lista, type(lista))
+print(len(lista))
+lista = ['back']
+print(lista, type(lista))
+print(len(lista))
+lista.append('front')
+print(lista, type(lista))
+print(len(lista))
+lista.append('data')
+print(lista, type(lista))
+print(len(lista)) '''
+#           0        1      2    3    4
+# REVERSE  -5       -4    -3   -2    -1
+lista = [ 'back', 'tarde', 21, True, 8.8 ]
+print(f' a quantidade de alunas na turma é: {lista[2]}')
+lista[2] = 22
+print(lista)
+lista[1] = False
+print(lista)
+lista[1] = ['Neyva', 'Alice', 'Lara', 'Paula', 'Geisa']
+print(lista[1][2])
+print(lista)
+del lista[-2]
+print(lista)
+del lista[-2]
+print(lista)
+del lista[-2]
+print(lista)
+lista.remove('back')
+print(lista)
+lista.append(26)
+lista.append(57)
+lista.append(900)
+print(lista)
+valor_do_pop = lista.pop()
+print(lista)
+print(valor_do_pop)
+print(lista)
+print(f'Foi removido da lista o cliente do id: {valor_do_pop}')
+lista.insert(0, 'Amontada Valley')
+print(lista)
+lista.insert(2, 'Professor')
+print(lista)
+lista.insert(200, 'Aluno')
+print(lista)
