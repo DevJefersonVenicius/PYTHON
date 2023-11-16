c8f9f29d0764bd5993b000a9ddf705c827218e7d
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-16 15:09:03 -0300

    Adicionado conteúdo no arquivo lista_num.py

--- testes/lista_num.py
+++ testes/lista_num.py
@@ -1,7 +1,10 @@
 # Números multiplos de 4
 # Deletando números multiplos de 4
+# Removendo número especifico
 lista = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 lista_pares = lista[2::2]
 print(lista_pares)
 del lista[0::4]
+print(lista)
+lista.remove(19)
 print(lista)
