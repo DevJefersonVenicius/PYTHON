08cdb5037292c4da0d88252c75b36ab931e53ddf
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-21 14:03:20 -0300

    Adicionado contéudo no  arquivo atv4.py

--- aula11.20/atv4.py
+++ aula11.20/atv4.py
@@ -1,5 +1,5 @@
 # faça um programa que receba dois números iteiros e gere os números inteiros que estão no intervalo compreendido por eles
 num = int(input('Digite o número: '))
 num2 = int(input('Digite o número: '))
-for i in range(num, num2):
+for i in range(num, num2 + 1):
    print(i)
