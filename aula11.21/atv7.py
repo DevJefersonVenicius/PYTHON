8f5482da728735d569711c191d1bb8068e3e5e25
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-23 13:49:25 -0300

    Adicionado atv7 para correcao posteriormente

--- aula11.21/atv7.py
+++ aula11.21/atv7.py
@@ -2,4 +2,9 @@
 while True:
     temperatura1 = float(input('Digite a primeira temperatura: '))
     temperatura2 = float(input('Digite a segunda temperatura: '))
-    
+    maior = temperatura1 > temperatura2 or temperatura2 > temperatura1
+    menor = temperatura1 < temperatura2 or temperatura2 < temperatura1
+    if maior:
+        print(maior)
+    if menor:
+        print(menor)
