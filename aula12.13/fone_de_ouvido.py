2efaf33e90f9a106252f360e3287bf495ece81c1
Venicius Vieira <veniciusvieira90@gmail.com>
2023-12-14 14:40:56 -0300

    Adicionado alterações ao arquivo fonedeouvido

--- aula12.13/fone_de_ouvido.py
+++ aula12.13/fone_de_ouvido.py
@@ -1,16 +1,17 @@
 class FoneDeOuvido:
-    volume = 0
+    
     def get_volume(self):
-        return self.volume
+        print(f'Entrei no GET')
+        return self.__volume
     
     def set_volume(self, novo_volume):
-        self.volume = novo_volume
+        print(f'Entrei no SET com o volume de {novo_volume}')
+        self.__volume = novo_volume
 
     volume = property(get_volume, set_volume)
 
 fone = FoneDeOuvido()
-
-fone.set_volume = 200
-fone.get_volume = 10
-print(fone.get_volume)
-print(fone.set_volume)
+fone.volume = 200 # SET
+print(fone.volume) # GET
+fone.set_volume(100) # GET
+print(fone.get_volume()) # SET
